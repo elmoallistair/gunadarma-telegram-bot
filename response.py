2db89fade40ad0edc6280cd062cf02929f405998
elmoallistair <work.elmoallistair@gmail.com>
2021-04-24 14:43:52 +0700

    update response
    improve response, fix wrong data order, add comment and exception, etc.

--- response.py
+++ response.py
@@ -1,14 +1,16 @@
 from collections import OrderedDict
+from datetime import datetime as dt
 from operator import getitem
 from pathlib import Path
 from telegram import ParseMode
+from textwrap import dedent
 import scraping
 import button
-import textwrap
 import yaml
 import re
 
 def validate_message(message):
+    """Check if message contains valid command"""
     list_of_commands = ["/start", "/help", "/kalendar", "/berita", "/jadwal",
                         "/jam_kuliah", "/cuti", "/non_aktif", "/cek_nilai",  
                         "/pindah_kelas", "/pindah_jurusan", "/loker"]
@@ -21,23 +23,29 @@ def validate_message(message):
         return None
 
 def load_data_from_yaml(command):
+    """Load data from yaml file"""
     yaml_path = f"scrape_files/data/{command[1:]}.yaml"
     data = yaml.load(open(yaml_path), Loader=yaml.FullLoader)
     return data
 
-def get_image_data(command):
-    file_path = f"scrape_files/data/{command[1:]}.yaml"
-    data = yaml.load(open(file_path), Loader=yaml.FullLoader)
-    return list(data.values())
+def sort_data_by_date_desc(data):
+    """Sort data in dict by date in descending order"""
+    key = lambda x: dt.strptime(getitem(x[1], "date"), "%d/%m/%Y")
+    sorted_data = OrderedDict(sorted(data.items(), key=key, reverse=True))
+    return sorted_data
 
 def template_from_file(command_or_callback, from_callback=False):
+    """Get template from html file"""
     if from_callback:
-        template=Path(f"response_templates/callback/{command_or_callback[1:-2]}.html").read_text()
+        template_path = f"response_templates/callback/{command_or_callback[1:-2]}.html"
     else:
-        template=Path(f"response_templates/command/{command_or_callback}.html").read_text()
+        template_path = f"response_templates/command/{command_or_callback}.html"
+
+    template=Path(template_path).read_text()
     return template
 
 def send_image(context, chat_id, image_path, keyboard=None, caption=None):
+    """Send image to user"""
     image = open(image_path, "rb")
     context.bot.sendPhoto(chat_id=chat_id, 
                           photo=image, 
@@ -45,67 +53,86 @@ def send_image(context, chat_id, image_path, keyboard=None, caption=None):
                           caption=caption, 
                           parse_mode=ParseMode.HTML)
 
-def create_reply(update, context, command):
+def reply_message(update, context, command):
+    """Reply message to user"""
     chat_id = update.message.chat.id
-    text = None
-    
+
     try:
         keyboard = button.create_button(command)
     except:
         keyboard = None
 
     if command == "/kalendar":
-        caption, image_path, _ = get_image_data(command)
+        text = None 
+        caption, image_path, _ = load_data_from_yaml(command).values()
         send_image(context, chat_id, image_path, keyboard, caption)
     elif command == "/jam_kuliah":
-        text = "Jadwal Kuliah"
-        image_path = get_image_data(command)[0]
+        image_path = load_data_from_yaml(command)["img_path"]
         send_image(context, chat_id, image_path)
+        text = template_from_file(command) # for reply_text, will send text after media
+    elif command == "/loker":
+        data = load_data_from_yaml(command)
+        content = ""
+        sorted_data = sort_data_by_date_desc(data)
+        for id in sorted_data:
+            date, title, url = sorted_data[id].values()
+            content += f"🗞 <b>({date})  <a href='{url}'>{title}</a></b>\n"
+        text = template_from_file(command).format(content)
     elif command == "/jadwal":
-        message = update.message.text.lower()
-        message_split = message.split(" ", 1) 
-        if message_split[0] == command and len(message_split) > 1:
-            input = message_split[1]
-            update.message.reply_text(f"Mencari jadwal untuk input <b>{input}</b>...", parse_mode=ParseMode.HTML)
-            data = scraping.scraping_jadwal_kuliah(input)
-            if data:
-                img_path, caption = data
-                send_image(context, chat_id, img_path, keyboard, caption)
-            else:
-                text = f"Input '{input}' tidak ada dalam database untuk kategori kelas / dosen"
-        else:
-            text = template_from_file(command)
+        keyboard = None # remove keyboard, recreate when successful get 'jadwal' data
+        text = template_from_file(command)
+        message = update.message.text.lower() 
+        message_split = message.split(" ", 1) # split command and query
+        if message_split[0] == command and len(message_split) == 2: # accept in format '/command <query>'
+            query = message_split[1]
+            if len(query) >= 5:
+                update.message.reply_text(f"Mencari jadwal untuk input '{query}' ...")
+                try:
+                    data_jadwal = scraping.scraping_jadwal_kuliah(query)
+                except Exception as e:
+                    update.message.reply_text(dedent(f"""
+                        ERROR: Terdapat masalah saat mengakses BAAK\n
+                        {e}\n
+                        Silahkan hubungi @elmoallistair
+                        """), parse_mode=ParseMode.HTML)
+                    return None
+                if data_jadwal: # successful scraping
+                    img_path, caption = data_jadwal
+                    keyboard = button.create_button(command)
+                    try:
+                        text = None # remove text
+                        send_image(context, chat_id, img_path, keyboard, dedent(caption))
+                    except:
+                        text = f"""
+                                Terlalu banyak data yang ditemukan untuk input '{query}' 
+                                Silahkan gunakan nama kelas atau dosen yang lebih spesifik.
+                                """
+                else:
+                    text = f"Input '{query}' tidak ada dalam database untuk kategori kelas/dosen"
     elif command == "/berita":
         data = load_data_from_yaml(command)
         message = update.message.text.lower()
-        message_split = message.split(" ", 1) 
-        if message_split[0] == command and len(message_split) > 1:
-            input = message_split[1]
+        message_split = message.split(" ", 1) # split command and query
+        if message_split[0] == command and len(message_split) == 2: # accept in format '/command <query>'
+            query = message_split[1]
             try:
-                title = data[input]["title"]
-                content = data[input]["content"]
+                content, _, title, _ = data[query].values()
                 text = f"<b>{title.upper()}</b>\n\n{content}"
             except:
-                text = f"Tidak dapat menemukan berita dengan id: <b>{input}</b>"
+                keyboard = None # remove keyboard
+                text = f"Tidak dapat menemukan berita dengan id: {query}"
         else:
             content = ""
-            sorted_data = OrderedDict(sorted(data.items(), key=lambda x: getitem(x[1], "date"), reverse=True))
-            for id in sorted_data.keys():
+            sorted_data = sort_data_by_date_desc(data)
+            for id in sorted_data:
                 _, date, title, url = sorted_data[id].values()
                 content += f"🗞 <b>({date} - {id})  <a href='{url}'>{title}</a></b>\n"
             text = template_from_file(command).format(content)
-    elif command == "/loker":
-        data = load_data_from_yaml(command)
-        text = "<b>POSTINGAN LOKER TERBARU</b>\n\n"
-        sorted_data = OrderedDict(sorted(data.items(), key=lambda x: getitem(x[1], "date"), reverse=True))
-        print(sorted_data)
-        for id in sorted_data.keys():
-            date, title, url = sorted_data[id].values()
-            text += f"🗞 <b>({date})  <a href='{url}'>{title}</a></b>\n"
     else:
         text = template_from_file(command)
-    
-    update.message.reply_text(text=textwrap.dedent(text), 
-                              reply_markup=keyboard, 
-                              parse_mode=ParseMode.HTML, 
-                              disable_web_page_preview=True)
+
+    if text is not None:
+        update.message.reply_text(text=dedent(text), 
+                                  reply_markup=keyboard, 
+                                  parse_mode=ParseMode.HTML, 
+                                  disable_web_page_preview=True)
